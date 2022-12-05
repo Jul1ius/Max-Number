7d9e598da9231e7b711377ead74082f4f52332d7
its julii <nanji828@gmail.com>
2022-12-05 20:15:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            double finalNum = 0;
+            
+            while (num != "Stop")
+            {
+                double secNum = double.Parse(num);
+                if (secNum > finalNum)
+                {
+                    finalNum = secNum; 
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(finalNum);
+        }
+    }
+}
